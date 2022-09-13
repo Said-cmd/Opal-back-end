f368204b11fcfbea333f77897f473a31c92131d4
Said Hussein <saidhussein12342@gmail.com>
2022-09-13 19:34:26 +0300

    Wrote the CRUD operations in the application.rb file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,38 @@ class ApplicationController < Sinatra::Base
     get "/" do
         { message: "Backend up and running!" }.to_json
       end
-    
+
+    get "/transactions" do 
+      transactions= Transaction.all
+      transactions.to_json
+    end
+
+    post "/transactions" do 
+      transaction = Transaction.create(
+        date: params[:date],
+        description: params[:description],
+        amount: params[:amount],
+        category: params[:category],
+        user_id: 1
+      )
+      transaction.to_json
+    end
+
+    patch "/transactions/:id" do 
+      transaction = Transaction.find(params[:id])
+      transaction.update(
+        date: params[:date],
+        description: params[:description],
+        amount: params[:amount],
+        category: params[:category]
+        user_id: 1
+      )
+      transaction.to_json
+    end
+
+    delete '/transactions/:id' do 
+      transaction = Transaction.find(params[:id])
+      transaction.destroy
+      transaction.to_json
+    end
 end
